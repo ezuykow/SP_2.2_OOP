3a817e26a2a590fe0d78ea36366f267bf640128b
ezuykow <zuy1996@gmail.com>
2022-09-22 09:45:41 +0300

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,25 @@ public class Main {
         Human maxim = new Human();
         maxim.name = "Maxim";
         maxim.city = "Minsk";
+        maxim.jobTitle = "brand manager";
         maxim.yearOfBirth = LocalDate.now().getYear() - 35;
 
         Human anya = new Human();
         anya.name = "Anya";
         anya.city = "Moscow";
+        anya.jobTitle = "methodologist of education programs";
         anya.yearOfBirth = LocalDate.now().getYear() - 29;
 
         Human katya = new Human();
         katya.name = "Katya";
         katya.city = "Kaliningrad";
+        katya.jobTitle = "product manager";
         katya.yearOfBirth = LocalDate.now().getYear() - 28;
 
         Human artyom = new Human();
         artyom.name = "Artyom";
         artyom.city = "Moscow";
+        artyom.jobTitle = "director of business development";
         artyom.yearOfBirth = LocalDate.now().getYear() - 27;
 
         maxim.sayHello();

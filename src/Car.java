b01024c4c89ce5d50f64769e1b13c6aa9f512f52
ezuykow <zuy1996@gmail.com>
2022-09-22 10:30:17 +0300

    task 2.2 done

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     int yearOfProduction;
     double engineVolume;
 
+    public Car(String brand, String model, String color, String assemblyCountry, int yearOfProduction, double engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.assemblyCountry = assemblyCountry;
+        this.yearOfProduction = yearOfProduction;
+        this.engineVolume = engineVolume;
+    }
+
     @Override
     public String toString() {
         return String.format("%s %s, product in %s in %d, body color - %s, engine volume - %.1f",
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car();
-        lada.brand = "Lada";
-        lada.model = "Grande";
-        lada.yearOfProduction = 2015;
-        lada.assemblyCountry = "Russia";
-        lada.color = "yellow";
-        lada.engineVolume = 1.7F;
+        Car lada = new Car("Lada", "Grande", "yellow",
+                "Russia", 2015, 1.7F);
 
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.yearOfProduction = 2020;
-        audi.assemblyCountry = "Germany";
-        audi.color = "black";
-        audi.engineVolume = 3.0F;
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", "black",
+                "Germany", 2020, 3.0F);
 
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.assemblyCountry = "Germany";
-        bmw.yearOfProduction = 2021;
-        bmw.color = "black";
-        bmw.engineVolume = 3.0F;
+        Car bmw = new Car("BMW", "Z8", "black",
+                "Germany",  2021, 3.0F);
 
-        Car kia =  new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage mk4";
-        kia.yearOfProduction = 2018;
-        kia.assemblyCountry = "South Korea";
-        kia.color = "red";
-        kia.engineVolume = 2.4F;
+        Car kia =  new Car("Kia", "Sportage mk4", "red",
+                "South Korea", 2018, 2.4F);
 
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.assemblyCountry = "South Korea";
-        hyundai.color = "orange";
-        hyundai.engineVolume = 1.6F;
-        hyundai.yearOfProduction = 2016;
+        Car hyundai = new Car("Hyundai", "Avante", "orange",
+                "South Korea", 2016, 1.6F);
 
         System.out.printf("%s\n%s\n%s\n%s\n%s\n", lada, audi, bmw, kia, hyundai);
     }

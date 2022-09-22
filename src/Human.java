02b1501dc6df77ef935f1e9fc4f0681204f0d34d
ezuykow <zuy1996@gmail.com>
2022-09-22 09:39:59 +0300

    task 1 done

---
+++ src/Human.java
@@ -0,0 +1,12 @@
+public class Human {
+    String name;
+    String city;
+    int yearOfBirth;
+
+    void sayHello() {
+        System.out.println("Hello! My name is " + name +
+                ". I`m from " + city +
+                ". I`m was born in " + yearOfBirth +
+                ". Let`s get acquainted!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Human maxim = new Human();
+        maxim.name = "Maxim";
+        maxim.city = "Minsk";
+        maxim.yearOfBirth = LocalDate.now().getYear() - 35;
 
+        Human anya = new Human();
+        anya.name = "Anya";
+        anya.city = "Moscow";
+        anya.yearOfBirth = LocalDate.now().getYear() - 29;
+
+        Human katya = new Human();
+        katya.name = "Katya";
+        katya.city = "Kaliningrad";
+        katya.yearOfBirth = LocalDate.now().getYear() - 28;
+
+        Human artyom = new Human();
+        artyom.name = "Artyom";
+        artyom.city = "Moscow";
+        artyom.yearOfBirth = LocalDate.now().getYear() - 27;
+
+        maxim.sayHello();
     }
 }

fec15185a23ca6d5103cb5dbc595e684c14c9f18
ezuykow <zuy1996@gmail.com>
2022-09-22 11:17:31 +0300

    task 3.1 done

--- src/Human.java
+++ src/Human.java
@@ -1,14 +1,41 @@
+import java.sql.Struct;
+
 public class Human {
     String name;
     String city;
     String jobTitle;
     int yearOfBirth;
 
+    private static String d = "<no info>"; //default value
+    public Human() {
+        this(d, d, d, 0);
+    }
+
+    public Human(String name) {
+        this(name, d, d, 0);
+    }
+
+    public Human(String name, int yearOfBirth) {
+        this(name, d, d, yearOfBirth);
+    }
+
+    public Human(String name, String city) {
+        this(name, city, d, 0);
+    }
+
+    public Human(String name, String city, int yearOfBirth) {
+        this(name, city, d, yearOfBirth);
+    }
+
+    public Human(String name, String city, String jobTitle) {
+        this(name, city, jobTitle, 0);
+    }
+
     public Human(String name, String city, String jobTitle, int yearOfBirth) {
         this.name = name;
         this.city = city;
         this.jobTitle = jobTitle;
-        this.yearOfBirth = yearOfBirth;
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
     }
 
     void sayHello() {
